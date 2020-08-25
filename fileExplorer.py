ad1cd18380673773513ccf3bfb55c49f1d95c419
vishwassh <vishwassh@spiceworks.com>
2020-08-25 22:59:06 +0530

    added file explorer

---
+++ fileExplorer.py
@@ -0,0 +1,15 @@
+#%%
+import os
+
+#%%
+def findFiles():
+    print(os.getcwd())
+    print(os.getcwdb())
+    print(os.cpu_count())
+    print(os.getlogin())
+    print(dir(os))
+
+findFiles()
+#%%
+if __name__ == "__main__":
+    findFiles()
